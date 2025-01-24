da3f52c4d83a47a01a2a095e816e4d035bec9a11
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-24 16:00:23 -0500

    diff --git a/src/main.rs b/src/main.rs index cda9c1b..b0e37df 100644 --- a/src/main.rs +++ b/src/main.rs @@ -1,34 +1,56 @@  use dirs::*;  use std::env;  use tokio::time::{sleep, Duration}; +use tracing_log::log; +use std::error::Error; + +async fn get_blockheight() -> Result<String, Box<dyn Error>> { +    let client = reqwest::Client::builder() +        .build() +        .expect("should be able to build reqwest client"); +    let blockheight = client +        .get("https://mempool.space/api/blocks/tip/height") +        .send() +        .await?; +    log::debug!("mempool.space status: {}", blockheight.status()); +    if blockheight.status() != reqwest::StatusCode::OK { +        log::debug!("didn't get OK status: {}", blockheight.status()); +        Ok(String::from(">>>>>")) +    } else { +        let blockheight = blockheight.text().await?; +        log::debug!("{}", blockheight); +        Ok(blockheight) +    } +} +  #[tokio::main]  async fn main() -> std::io::Result<()> {      println!("tokio_multi:simple");      let cwd = env::current_dir().unwrap();      let cwd_to_string_lossy: String = String::from(cwd.to_string_lossy()); -    println!("{}", cwd_to_string_lossy); +    log::info!("{}", cwd_to_string_lossy);
        let local_data_dir = data_local_dir();
    -    println!("{}", local_data_dir.expect("REASON").display());
    +    log::info!("{}", local_data_dir.expect("REASON").display());
         let task_one = tokio::spawn(async {
    -        println!("Task one is started");
    -        sleep(Duration::from_secs(2)).await;
    -        println!("Task one is done");
    +        log::info!("Task one is started");
    +        eprint!("blockheight={}\n", get_blockheight().await.expect("blockheight"));
    +        sleep(Duration::from_secs(1)).await;
    +        log::info!("Task one is done");
         });
    
         let task_two = tokio::spawn(async {
    -        println!("Task two is started");
    -        sleep(Duration::from_secs(1)).await;
    -        //println!("Task two is done");
    +        log::info!("Task two is started");
    +        log::info!("serie.run().await");
             let _ = serie::run().await;
    -        println!("Task two is done");
    +        log::info!("Task two is done");
         });
    
         // Await the tasks to complete
    -    //let _ = task_one.await.unwrap();
    -    let _ = task_two.await.unwrap();
         let _ = task_one.await.unwrap();
    -Ok(())
    +    let _ = task_two.await.unwrap();
    +    //let _ = task_one.await.unwrap();
    +    Ok(())
    
         //serie::run()
     }

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
 use dirs::*;
 use std::env;
 use tokio::time::{sleep, Duration};
+use tracing_log::log;
+use std::error::Error;
+
+async fn get_blockheight() -> Result<String, Box<dyn Error>> {
+    let client = reqwest::Client::builder()
+        .build()
+        .expect("should be able to build reqwest client");
+    let blockheight = client
+        .get("https://mempool.space/api/blocks/tip/height")
+        .send()
+        .await?;
+    log::debug!("mempool.space status: {}", blockheight.status());
+    if blockheight.status() != reqwest::StatusCode::OK {
+        log::debug!("didn't get OK status: {}", blockheight.status());
+        Ok(String::from(">>>>>"))
+    } else {
+        let blockheight = blockheight.text().await?;
+        log::debug!("{}", blockheight);
+        Ok(blockheight)
+    }
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     println!("tokio_multi:simple");
     let cwd = env::current_dir().unwrap();
     let cwd_to_string_lossy: String = String::from(cwd.to_string_lossy());
-    println!("{}", cwd_to_string_lossy);
+    log::info!("{}", cwd_to_string_lossy);
 
    let local_data_dir = data_local_dir();
-    println!("{}", local_data_dir.expect("REASON").display());
+    log::info!("{}", local_data_dir.expect("REASON").display());
     let task_one = tokio::spawn(async {
-        println!("Task one is started");
-        sleep(Duration::from_secs(10)).await;
-        println!("Task one is done");
+        log::info!("Task one is started");
+        eprint!("blockheight={}\n", get_blockheight().await.expect("blockheight"));
+        sleep(Duration::from_secs(1)).await;
+        log::info!("Task one is done");
     });
 
     let task_two = tokio::spawn(async {
-        println!("serie.run().await");
+        log::info!("Task two is started");
+        log::info!("serie.run().await");
         let _ = serie::run().await;
-        sleep(Duration::from_secs(1)).await;
-        println!("Task two is done");
+        log::info!("Task two is done");
     });
 
     // Await the tasks to complete
-    //let _ = task_one.await.unwrap();
-    let _ = task_two.await.unwrap();
     let _ = task_one.await.unwrap();
+    let _ = task_two.await.unwrap();
+    //let _ = task_one.await.unwrap();
     Ok(())
 
     //serie::run()
